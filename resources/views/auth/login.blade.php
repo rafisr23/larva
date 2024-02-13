22ce919114701ca5b18c081bcd25211630a1ab62
Abdur Rafi <rafiridwan23@gmail.com>
2024-02-13 16:14:13 +0700

    feat(auth): login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,14 +4,14 @@
         <div class="absolute inset-0">
             <img src="/images/backgrounds/login-bg.png" alt="image" class="h-full w-full object-cover" />
         </div>
-        <div class="relative flex min-h-screen items-center justify-center bg-[url(/images/backgrounds/login-bg.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
+        <div class="relative flex min-h-screen max-h-screen items-center justify-center bg-[url(/images/backgrounds/login-bg.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
             {{-- <img src="/images/auth/coming-soon-object1.png" alt="image" class="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
             <img src="/images/auth/coming-soon-object2.png" alt="image" class="absolute left-24 top-0 h-40 md:left-[30%]" />
             <img src="/images/auth/coming-soon-object3.png" alt="image" class="absolute right-0 top-0 h-[300px]" />
             <img src="/images/auth/polygon-object.svg" alt="image" class="absolute bottom-0 end-[28%]" /> --}}
             <div
                 class="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]">
-                <div class="relative flex flex-col justify-center rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[758px] py-20">
+                <div class="relative flex flex-col justify-center rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[558px] py-10">
                     {{-- <div class="absolute top-6 end-6">
                         <div class="dropdown" x-data="dropdown" @click.outside="open = false">
                             <a href="javascript:;" class="flex items-center gap-2.5 rounded-lg border border-white-dark/30 bg-white px-2 py-1.5 text-white-dark hover:border-primary hover:text-primary dark:bg-black"
